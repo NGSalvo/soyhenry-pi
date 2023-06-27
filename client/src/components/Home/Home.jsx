588e8d7f7a0473cf24c8ee63b9816ceb4c7a1649
Nicolás Salvo <nicosalvo@gmail.com>
2023-06-27 18:24:48 -0300

    style: nav refactor: move filters and ordering selects to home style: add some css variables

--- client/src/components/Home/Home.jsx
+++ client/src/components/Home/Home.jsx
@@ -4,6 +4,7 @@ import { Cards, Pagination, TemperamentFilter } from "@components";
 import { useDispatch } from 'react-redux';
 import { filterByTemperament } from "../../redux/actions";
 import { useState } from 'react';
+import { clearCards, filterCards, orderCards } from "../../redux/actions";
 
 export const Home = ({ dogs, currentPage, totalPages, getCurrentItems, navigateToPage }) => {
   const dispatch = useDispatch()
@@ -17,11 +18,41 @@ export const Home = ({ dogs, currentPage, totalPages, getCurrentItems, navigateT
     isHidden ? setHidden(false) : setHidden(true)
   }
 
+  const handleFilter = (event) => {
+    dispatch(filterCards(event.target.value))
+    navigateToPage(1)
+  }
+
+  const handleOrder = (event) => {
+    dispatch(orderCards(event.target.value))
+  }
+
+  const handleClearAll = () => {
+    dispatch(clearCards())
+    navigateToPage(1)
+  }
+
   return (
     <div className={style.container}>
+    <button onClick={handleClearAll}>Clear 🪄</button>
+      Created At: 
+      <select onChange={handleFilter}>
+        <option value="ALL">ALL</option>
+        <option value="API">API</option>
+        <option value="DB">DB</option>
+      </select>    
+      Order by:
+      <select onChange={handleOrder}>
+        <option value="ASC">Ascending</option>
+        <option value="DESC">Descending</option>
+        <option value="HEAVIER">Heavier</option>
+        <option value="LIGHTER">Lighter</option>
+        <option value="AZ">A-Z</option>
+        <option value="ZA">Z-A</option>
+      </select>  
       <button className={style['btn-filter']} onClick={handleToggleHideFilter}>
         {
-          isHidden ? 'Ocultar filtros' : 'Mostrar filtros'
+          isHidden ? 'Hide filters' : 'Show filters'
         }
       </button>
       <div className={style['center-self']}>
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,53 +1,21 @@
-import { useDispatch } from "react-redux";
 import { NavLink } from 'react-router-dom';
 
 import { SearchBar } from "@components";
-import { clearCards, filterCards, orderCards } from "../../redux/actions";
+
 import style from "./Nav.module.css";
 
-export const Nav = ({ onSearch, navigateToPage }) => {
-  const dispatch = useDispatch()
-  const handleFilter = (event) => {
-    dispatch(filterCards(event.target.value))
-    navigateToPage(1)
-  }
-
-  const handleOrder = (event) => {
-    dispatch(orderCards(event.target.value))
-  }
-
-  const handleClearAll = () => {
-    dispatch(clearCards())
-    navigateToPage(1)
-  }
-
+export const Nav = ({ onSearch }) => {
   return (
-    <div className={style.container}>
+    <nav className={style.container}>
       <div>
         <NavLink to={'/home'} className={({isActive}) => isActive ? style.active: style.inactive}>
-          Inicio
+          Home 🏠
         </NavLink>
         <NavLink to={'/create'} className={({isActive}) => isActive ? style.active: style.inactive}>
-          Agregar 🐶
-        </NavLink>
-        <button onClick={handleClearAll}>Limpiar todo</button>
-        Fuente: 
-        <select onChange={handleFilter}>
-          <option value="ALL">DB y API</option>
-          <option value="DB">DB</option>
-          <option value="API">API</option>
-        </select>    
-        Ordernar por:
-        <select onChange={handleOrder}>
-          <option value="ASC">Ascendente</option>
-          <option value="DESC">Descendente</option>
-          <option value="HEAVIER">Más Pesado</option>
-          <option value="LIGHTER">Más Liviano</option>
-          <option value="AZ">A-Z</option>
-          <option value="ZA">Z-A</option>
-        </select>    
+          Add 🐶
+        </NavLink>  
       </div>
       <SearchBar onSearch={onSearch} />
-    </div>
+    </nav>
   )
 }
