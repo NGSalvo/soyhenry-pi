c2e4d5061c92f9549efd1185c2d84f85273ac966
Nicolás Salvo <nicosalvo@gmail.com>
2023-06-28 10:46:30 -0300

    style: card add animated line

--- client/src/components/Card/Card.jsx
+++ client/src/components/Card/Card.jsx
@@ -4,13 +4,14 @@ import noDogImage from '../../assets/images/noDogImage.webp'
 
 import { Temperament } from "@components";
 
-// export const Card = ({id, name, status, species, gender, origin, image, onClose, myFavorites, addFav, removeFav}) => {
+
 export const Card = ({id, name, weight, temperaments, image}) => {
    return (
       <div className={style.container}>
          <img src={image.endsWith('webp') ? noDogImage : image} alt={name} />
          <div>
             <h2>{name}</h2>
+            <hr />
             <h5>Weight: {weight}</h5>
             <h5>Temperament: <Temperament temperaments={temperaments}/></h5>
          </div>
